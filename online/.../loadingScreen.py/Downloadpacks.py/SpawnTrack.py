ce8cdd40b99b7b745b2c17614cdfd48fe4f8b43b
FastboiStudios <121701084+FastboiStudios@users.noreply.github.com>
2023-11-10 12:11:52 -0500

    Update SpawnTrack.py

--- online/.../loadingScreen.py/Downloadpacks.py/SpawnTrack.py
+++ online/.../loadingScreen.py/Downloadpacks.py/SpawnTrack.py
@@ -1,10 +1,27 @@
-import os
-
-f = open("SpriteReadyup.py", "a")
-f.write("import OS\nos.startup = github_IMG")
-f.close()
-
-#open and read the file after the appending:
-f = open("SpriteReadyup", "r")
-print(f.read())
-open ('Downloadpacks.py')
+input ('You: w-were am I? press enter to pass diuloge')
+input ('You: i will investigate...')
+print ('Walk around. sneek by pressing shift. sneeking helps you from being heard.')
+# importing module
+import time
+ 
+ 
+# running loop from 0 to 4
+for i in range(0,5):
+   
+  # printing numbers
+  print(i)
+   
+  # adding 2 seconds time delay
+  time.sleep(10)
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+  
+open ('Spawn.png')

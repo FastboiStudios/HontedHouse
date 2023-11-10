f387c1d1fee7a343ce20a5efe2efc76b2086e62b
FastboiStudios <121701084+FastboiStudios@users.noreply.github.com>
2023-11-10 12:03:15 -0500

    Create SpawnTrack.py

---
+++ online/.../loadingScreen.py/Downloadpacks.py/SpawnTrack.py
@@ -0,0 +1,10 @@
+import os
+
+f = open("SpriteReadyup.py", "a")
+f.write("import OS\nos.startup = github_IMG")
+f.close()
+
+#open and read the file after the appending:
+f = open("SpriteReadyup", "r")
+print(f.read())
+open ('Downloadpacks.py')

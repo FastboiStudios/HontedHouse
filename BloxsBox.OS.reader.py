84f699b35615de905d7e86e0fbdeef4869865542
FastboiStudios <121701084+FastboiStudios@users.noreply.github.com>
2023-11-10 12:32:02 -0500

    Create BloxsBox.OS.reader.py

---
+++ BloxsBox.OS.reader.py
@@ -0,0 +1,16 @@
+import os
+
+print ('"BloxsBox.lsp" has been listed for main OS')
+BoostRam = input ('Boost RAM for this game up 2? Y/N')
+if  boostRam == 'y' or 'Y'
+ print ('RAM boosted')
+
+ f = open("GameRamBoost.UI", "a")
+f.write("RamBoosterApps: HontedHouse, ")
+f.close()
+
+#open and read the file after the appending:
+f = open("GameRamBoost.UI", "r")
+print(f.read())
+
+open ('SpawnTracker.py')
